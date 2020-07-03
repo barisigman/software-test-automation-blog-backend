2ba5aeb67f5a640470869f33d196a61b2cf48640
Baris Igman <bigman924@hotmail.com>
2020-07-03 23:15:17 +0300

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:node/recommended',
+    'plugin:security/recommended',
+  ],
+  env: {
+    commonjs: true,
+    browser: true,
+    node: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'node/exports-style': ['error', 'module.exports'],
+    'node/file-extension-in-import': ['error', 'always'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
+  },
+};
